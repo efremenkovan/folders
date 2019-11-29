030ec931709763d068b3d5944b2d015e0fd03679
Efremenkov Andrey <efremenkovan99@gmail.com>
2019-11-30 00:12:20 +0300

    Added --help flag.

--- folder.py
+++ folder.py
@@ -3,13 +3,22 @@ from termcolor import colored
 import os
 import sys 
 import re
+from os import system, name
+
 DEPTH_LEVEL = sys.maxsize
+
 
 def checkExistance(string,array):
 	for item in array:
 		if re.match(string,item) != None:
 			return False
 	return True
+
+def helpPrint():
+	print("Currenly avaliable flags for fold CLI:\n")
+	print("--depth number        Number represents how deeph you want to look\n                      in your folder. 0 - print only directly nested folders and files")
+	print("--ignonre folder      Folder - name of the folder you don't want to print.\n                      In example node_modules. Currenly you can ignore only 1 folder")
+	print("\n")
 
 def main(enter,loop, preset,toIgnore):
 	if loop <= DEPTH_LEVEL+1:
@@ -48,6 +57,9 @@ if __name__ == '__main__':
 			IGNORED_FOLDER.append(str(sys.argv[index+1]))
 		elif arg=="--depth":
 			DEPTH_LEVEL = int(sys.argv[index+1],base=10)
+		elif arg=="--help":
+			helpPrint()
+			sys.exit()
 	print(colored('\nFolder: '+os.getcwd().split('/')[-1].upper(),'green'),'\n|')
 	main(os.getcwd(),1,'',IGNORED_FOLDER)
 	print('\n')
